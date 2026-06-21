6c6c69918bd0ff524266c03e188f585b3c45b875
arookieofc <2128194521@qq.com>
2026-06-22 05:38:11 +0800

    feat: support hash output redirection

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -4773,6 +4773,31 @@ impl Executor {
     fn execute_hash(&mut self, cmd: &CommandNode) -> Result<i32, ExecuteError> {
         // TODO(redir.c/builtins/hash.def): Redirections are command-level in
         // Bash. This covers `hash -t cat 2>/dev/null` from builtins9.sub.
+        if let Some(redirect) = &cmd.redirect_out {
+            let target = self.expand_word(&redirect.target);
+            let mut file = File::create(shell_path_to_windows(&target, &self.env_vars))?;
+            return Ok(crate::builtins::hash::execute_with_io(
+                &cmd.words[1..],
+                &mut self.env_vars,
+                &mut file,
+                &mut std::io::stderr().lock(),
+            )?);
+        }
+
+        if let Some(redirect) = &cmd.append {
+            let target = self.expand_word(&redirect.target);
+            let mut file = OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(shell_path_to_windows(&target, &self.env_vars))?;
+            return Ok(crate::builtins::hash::execute_with_io(
+                &cmd.words[1..],
+                &mut self.env_vars,
+                &mut file,
+                &mut std::io::stderr().lock(),
+            )?);
+        }
+
         if let Some(redirect) = &cmd.redirect_err {
             let target = self.expand_word(&redirect.target);
             if is_null_device(&target) {
