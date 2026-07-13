2e3b6b35daef3e190f05ccb51cabe270fe0988c0
arookieofc <2128194521@qq.com>
2026-07-13 09:22:14 +0800

    feat: cover negated extglob conditionals

--- tests/executor_command_chaining/part_072.rs
+++ tests/executor_command_chaining/part_072.rs
@@ -43,6 +43,31 @@ fn test_conditional_string_equality_uses_shell_patterns() {
 }
 
 #[test]
+fn test_conditional_string_equality_uses_negated_extglob_patterns() {
+    let output_path = "target/rubash-conditional-negated-extglob-output.txt";
+    let _ = fs::remove_file(output_path);
+    let input = format!(
+        "shopt -s extglob; \
+         [[ file.txt == !(*.tmp) ]]; echo txt:$? > {output_path}; \
+         [[ file.tmp == !(*.tmp) ]]; echo tmp:$? >> {output_path}; \
+         [[ file.tmp != !(*.tmp) ]]; echo not_tmp:$? >> {output_path}"
+    );
+    let tokens = tokenize(&input);
+    let ast = parse(&tokens);
+    let mut executor = Executor::new();
+
+    let result = executor.execute_ast(&ast);
+
+    assert!(result.is_ok());
+    assert_eq!(executor.last_exit_code(), 0);
+    assert_eq!(
+        fs::read_to_string(output_path).unwrap(),
+        "txt:0\ntmp:1\nnot_tmp:0\n"
+    );
+    let _ = fs::remove_file(output_path);
+}
+
+#[test]
 fn test_conditional_regex_match_sets_bash_rematch() {
     let output_path = "target/rubash-conditional-regex-output.txt";
     let _ = fs::remove_file(output_path);
